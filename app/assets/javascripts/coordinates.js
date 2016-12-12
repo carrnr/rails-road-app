f0f57d18126d5e55121b0f96b93e7a459f7e7ba9
Natalie Dodd <nataliedodd89@gmail.com>
2016-12-12 09:27:28 -0500

    added some basic css and added reset function on create new map

--- app/assets/javascripts/coordinates.js
+++ app/assets/javascripts/coordinates.js
@@ -1,60 +1,7 @@
 $(function() {
 
-  // $('#view-maps').on('click', function(event) {
-  //   event.preventDefault();
-  //
-  //   var point1
-  //   var point2
-  //
-  //   $.ajax({
-  //     url:'/maps',
-  //     method:'GET',
-  //     dataType:'JSON',
-  //     data: {}
-  //   }).done(function(response){
-  //     //  for (var i=0; i < response.length; i++) {
-  //        point1 = response[0].coordinates[0];
-  //        point2 = response[0].coordinates[1];
-  //
-  //        console.log(point1);
-  //        console.log(point2);
-  //     //  }
-  //   });
-  //
-  //   initMap = function() {
-  //
-  //       var mapOptions = {
-  //         center: {lat: point1[0], lng: point1[1]},
-  //         zoom: 13
-  //       };
-  //
-  //       var myCoordinates = [
-  //         new google.maps.LatLng(point1),
-  //         new google.maps.LatLng(point2),
-  //       ];
-  //
-  //       var map2 = new google.maps.Map(document.getElementById('map2'), mapOptions);
-  //
-  //       var poly = new google.maps.Polyline({
-  //         path: myCoordinates,
-  //         strokeColor: '#ba0b0b',
-  //         strokeOpacity: 1.0,
-  //         strokeWeight: 5
-  //       });
-  //
-  //       poly.setMap(map2);
-  //
-  //     }
-  //
-  //     function addLatLng(event) {
-  //        var path = poly.getPath();
-  //
-  //        // Because path is an MVCArray, we can simply append a new coordinate
-  //        // and it will automatically appear.
-  //        path.push(event.latLng);
-  //     }
-  //
-  // });
-
+  $('#reset-map').on('click', function() {
+    initMapNew();
+  });
 
 });
