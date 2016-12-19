6def336dbcbaa772f3ee40f485ffa48d34dda80f
Natalie Dodd <nataliedodd89@gmail.com>
2016-12-19 09:30:33 -0500

    moved access control origin statement inside the RoadApp module

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,13 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => '*',
-    'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
-  }
-
 module RoadApp
   class Application < Rails::Application
+
+    config.action_dispatch.default_headers = {
+        'Access-Control-Allow-Origin' => '*',
+        'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+      }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
